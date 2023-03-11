8bc97257448e9898e1730311389e2845e5af8e1c
nazarena rodriguez <nazarena.rodriguez.sc@gmail.com>
2023-03-11 17:37:25 -0300

    promedio

---
+++ promedio.py
@@ -0,0 +1,18 @@
+a=int(input("ingrese cinco numeros: "))
+# b=int(input("ingrese cinco numeros: "))
+# c=int(input("ingrese cinco numeros: "))
+# d=int(input("ingrese cinco numeros: "))
+# e=int(input("ingrese cinco numeros: "))
+
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+ #calcular el promedio 
+s= a+b+c+d+e
+print("la suma de los numerosd es: ", s)
+print ("el promedio es",s/s)
+
+
+
+
