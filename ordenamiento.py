d8a67fe297f8b6e0c418f3d90d51778e7e4ddcf2
diego rodriguez <diego.rodriguez@teledoc.cl>
2023-03-11 16:55:23 -0300

    ordenamiento terminado

---
+++ ordenamiento.py
@@ -0,0 +1,19 @@
+#ingreso de numeros
+a=int(input("ingrese un numero : "))
+b=int(input("ingrese un numero : "))
+c=int(input("ingrese un numero : "))
+
+#ordenamiento
+lista = [a,b,c]
+aux=0
+
+for i in range (0, len(lista)):
+    for j in range(i + 1, len(lista)):
+        if(lista[i] > lista[j]):
+            aux=(lista[i])
+            lista[i]=(lista[j])
+            lista[j]=(aux)
+
+
+
+print(lista)
